6f8be5de7b54528d1fe8ab79a4e7bba5db94fe5d
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2024-12-22 22:34:50 +0600

    add inheritance private member access using seeter and gethear method

---
+++ inheritance/src/main/java/privatemember/Teachear.java
@@ -0,0 +1,14 @@
+
+package privatemember;
+
+public class Teachear extends Persion {
+    private String qulification;
+
+    public String getQulification() {
+        return qulification;
+    }
+
+    public void setQulification(String qulification) {
+        this.qulification = qulification;
+    }    
+}
---
+++ inheritance/src/main/java/privatemember/Test.java
@@ -0,0 +1,18 @@
+
+package privatemember;
+
+public class Test {
+    public static void main(String args [])
+    {
+        Teachear t1 = new Teachear();
+        t1.setName("topu");
+        t1.setQulification("Hsc");
+        t1.setAge(23);
+        System.out .println("Name is:"+t1.getName());
+        System.out .println("Age is :"+t1.getAge());
+        System.out .println("Qulification is:"+t1.getQulification());
+        
+        
+    }
+    
+}
